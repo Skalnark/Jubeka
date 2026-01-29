05f87abe76c4b4aef01ff82480f20c44eb006356
Skalnark <darciobasilio@gmail.com>
2026-01-29 14:57:53 -0300

    wip: update IUriBuilderHelper interface to include TODOs for nullable URI and additional method

--- Jubeka.Core/Application/IUriBuilderHelper.cs
+++ Jubeka.Core/Application/IUriBuilderHelper.cs
@@ -2,5 +2,9 @@ namespace Jubeka.Core.Application;
 
 public interface IUriBuilderHelper
 {
+    // TODO: make URI nullable
     Uri Build(string rawUrl, IReadOnlyDictionary<string, string> vars, IReadOnlyList<(string Key, string Value)> queryParams);
+
+    // TODO: add this method to the interface
+    //Uri? BuildFromUrl(string rawUrl, IReadOnlyDictionary<string, string> vars);
 }
