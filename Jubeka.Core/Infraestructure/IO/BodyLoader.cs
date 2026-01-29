b9910e75e6437763eafdb29a735e9359210bf136
Skalnark <darciobasilio@gmail.com>
2026-01-29 10:40:13 -0300

    wip: add BodyLoader class to handle loading of body content from files or strings

---
+++ Jubeka.Core/Infraestructure/IO/BodyLoader.cs
@@ -0,0 +1,26 @@
+using Jubeka.Core.Application;
+
+namespace Jubeka.Core.Infraestructure.IO;
+
+public sealed class BodyLoader : IBodyLoader
+{
+    public string Load(string? args)
+    {
+        if (string.IsNullOrEmpty(args))
+        {
+            return string.Empty;
+        }
+
+        if(args.StartsWith("@", StringComparison.Ordinal))
+        {
+            string filePath = args[1..];
+            if(!File.Exists(filePath))
+            {
+                throw new FileNotFoundException("Body file not found.", filePath);
+            }
+
+            return File.ReadAllText(filePath);
+        }
+        return args;
+    }
+}
